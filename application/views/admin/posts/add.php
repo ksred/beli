ea9d81927875b86baafaf2944e337f516d3c0aca
Kyle Redelinghuys <kyle@motribe.com>
2012-08-17 16:23:07 +0200

    Post adding

--- application/views/admin/posts/add.php
+++ application/views/admin/posts/add.php
@@ -1,5 +1,15 @@
 <?php $this->load->view("_template/header"); ?>
 
 <?php $this->load->view("admin/posts/menu"); ?>
-        
+
+<div class="span9">
+        <form method="POST" action="<?= BASE_URL ?>/admin_posts/_add" class="form-vertical post-add">
+            <input type="text" name="title" placeholder="Title" required /><br />
+            <textarea name="summary" placeholder="Summary" required></textarea><br />
+            <textarea rows=30 name="body" placeholder="Body" required></textarea><br />
+            <span>Multi select for categories</span>
+            <span>Multi select for tags</span>
+            <input type="submit" value="Create" class="btn">
+        </form>
+</div>
 <?php $this->load->view("_template/footer"); ?>
