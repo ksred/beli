d0a093f519864b80cdf4228147efff836e23bbca
Kyle Redelinghuys <kyle@motribe.com>
2012-08-20 14:44:55 +0200

    Front page, styling

--- application/views/front_page.php
+++ application/views/front_page.php
@@ -1,11 +1,11 @@
 <?php $this->load->view("_template/header-web"); ?>
 
 <div class="row-fluid">
-	<div class="span2">
+	<div class="span2 menu">
 		<?php $this->load->view("_template/nav-web"); ?>
 	</div>
 
-	<div class="span10">
+	<div class="span10 offset2 main">
 		<p>This is where content will go</p>
 	</div>
 
